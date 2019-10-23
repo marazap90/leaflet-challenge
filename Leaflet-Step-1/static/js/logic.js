b2006aed4455c4907186f923400633555fed88fc
marekz <marekzapletal.mz@gmail.com>
2019-10-23 17:57:48 -0500

    ad

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,3 +1,4 @@
+const API_KEY = "pk.eyJ1IjoibWFyYXphcCIsImEiOiJjazA5dG5qcm4wYmdxM25td3Vjb3hqZHV3In0.H4yMhzk2CLWelwV4OVx1jg";
 
 
 // function to make markersize dynamic based on value given (i.e. magnitude)
